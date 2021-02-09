0748a71a3ade38ae23e06113a64ff3c696ae9df1
nafiz1001 <nafiz.islam1001@gmail.com>
2021-02-09 11:13:44 -0500

    test approval code persistence

--- todopal-backend/src/test/java/todopal/dao/TestTeacherPersistence.java
+++ todopal-backend/src/test/java/todopal/dao/TestTeacherPersistence.java
@@ -22,6 +22,7 @@ public class TestTeacherPersistence {
 	private final String TEACHER_NAME = "NAME_TEST";
 	private final String TEACHER_EMAIL = "name.test@yahoo.ca";
     private final String TEACHER_BIO = "I love pen island!";
+	private final String TEACHER_APPROVAL_CODE = "911";
 
 	@Autowired
 	private PersonRepository personRepository;
@@ -41,6 +42,7 @@ public class TestTeacherPersistence {
 		teacher.setEmail(TEACHER_EMAIL);
 		teacher.setName(TEACHER_NAME);
 		teacher.setBio(TEACHER_BIO);
+		teacher.setApprovalCode(TEACHER_APPROVAL_CODE);
 
 		teacherRepository.save(teacher);
 		Teacher savedTeacher = teacherRepository.findByEmail(TEACHER_EMAIL);
@@ -48,6 +50,7 @@ public class TestTeacherPersistence {
 		assertNotNull(savedTeacher);
 		assertEquals(TEACHER_NAME, savedTeacher.getName());
 		assertEquals(TEACHER_BIO, savedTeacher.getBio());
+		assertEquals(TEACHER_APPROVAL_CODE, savedTeacher.getApprovalCode());
 	}
 
 }
