3bfa6df72032d6b508e58d494ee2bdf4ca037fb2
Daniel <danielbucci2@gmail.com>
2021-02-14 17:33:02 -0500

    creates TestCreateTeacherAccount test class

--- todopal-backend/src/test/java/todopal/features/TestCreateTeacher.java
+++ todopal-backend/src/test/java/todopal/features/TestCreateTeacher.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 
 import io.cucumber.java.en.*;
 import todopal.controller.TodopalRestController;
+import todopal.model.Teacher;
 import todopal.service.TeacherService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,18 +15,34 @@ public class TestCreateTeacher {
 	 TodopalRestController todopalRestController;
 	 @Autowired
 	 private TeacherService teacherService;
+	 
+	 @Given("an unregistered teacher wants to register for a new account")
+	 public void an_unregistered_teacher_wants_to_register_for_a_new_account() {
+	     
+	 }
+
+	 @When("user {string} with email {string} registers for a teacher account with name {string} and password {string}")
+	 public void user_with_email_registers_for_a_teacher_account_with_name_and_password(String name, String email, String name2, String password) {
+		teacherService.createTeacher("123", name, email, password, "bio empty");
+	 }
+	 
+	 @Then("an account for {string} is created under the email {string} with name {string} and password {string}")
+	 public void an_account_for_is_created_under_the_email_with_name_and_password(String email, String email2, String name, String password) {
+	     Teacher teacher = teacherService.getTeacher(email);
+	     Assertions.assertNotNull(teacher);
+	 }
+	 
+	 @Given("a registered teacher {string} with email {string} wants to register for a new account")
+	 public void a_registered_teacher_with_email_wants_to_register_for_a_new_account(String name, String email) {
+		 teacherService.createTeacher("123", name,email, "password", "bio empty");
+	 }
+
+	 @Then("an {string} message is issued")
+	 public void an_message_is_issued(String errorMessage) {
+		 
+	    Assertions.assertEquals(errorMessage, "");
+	    
+	 }
 	
-//	@Given("An unregistered teacher wants to register for a new account")
-//	public void an_unregistered_teacher_wants_to_register_for_a_new_account() {
-//		
-//	}
-//	
-//	@When("user {String} with email {String} registers for a teacher account with name {String} and password {String}")
-//	public void user_with_email_registers_for_a_teacher_account_with_name_and_password(String email, String name, String password) {
-//		todopalRestController.createTeacher("123", name, email, password, "bio empty");
-//	}
-//	
-//	@Then("Then an account for {String} is created under the email {String} with name <name> and password {String}")
-//	public void 
 	
 }
--- todopal-backend/src/test/resources/ID001_Create_Teacher_Account.feature
+++ todopal-backend/src/test/resources/ID001_Create_Teacher_Account.feature
@@ -8,16 +8,16 @@ Scenario Outline: Different Teachers (Normal Flow)
 
     Given an unregistered teacher wants to register for a new account
     When user "Michael Scott" with email "michael.scott@gmail.com" registers for a teacher account with name "Michael Scott" and password "aa001"
-    Then an account for "Michael Scott" is created under the email "michael.scott@gmail.com" with name "Michael Scott" and password "aa001"
+    Then an account for "michael.scott@gmail.com" is created under the email "michael.scott@gmail.com" with name "Michael Scott" and password "aa001"
 
 Scenario Outline: Teacher attempts to become an user with an invalid email (Error Flow)
 
     Given an unregistered teacher wants to register for a new account
     When user "Michael Scarn" with email "INVALID_EMAIL" registers for a teacher account with name "Real_Michael" and password "1234"
-    Then an "Invalid email is used" message is issued
+    Then a "Invalid email is used" message is issued
 
 Scenario Outline: Existing teacher attempts to become a user (Error Flow)
 
     Given a registered teacher "Golden Face" with email "g.face@gmail.com" wants to register for a new account
     When user "Golden Face" with email "g.face@gmail.com" registers for a teacher account with name "GFace" and password "1234"
-    Then an "Already registered" message is issued
+    Then a "Already registered" message is issued
