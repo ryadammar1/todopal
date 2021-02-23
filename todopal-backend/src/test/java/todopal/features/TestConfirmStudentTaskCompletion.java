b815b5e1584a0aba2bbfc40a0e4143ebc7139567
nafiz1001 <nafiz.islam1001@gmail.com>
2021-02-23 18:27:30 -0500

    pass ID009 feature

--- todopal-backend/src/test/java/todopal/features/TestConfirmStudentTaskCompletion.java
+++ todopal-backend/src/test/java/todopal/features/TestConfirmStudentTaskCompletion.java
@@ -46,44 +46,54 @@ public class TestConfirmStudentTaskCompletion {
 
     @Given("there is a class task called {string} that is worth {string} points")
     public void there_is_a_class_task_called_that_is_worth_points(String taskName, String points) {
-        final Task task = taskService.createTask(0, taskName, "", "chem", "mandatory", true, Integer.parseInt(points),
+        final Task task = taskService.createTask(1, taskName, "", "chem", "mandatory", true, Integer.parseInt(points),
                 LocalDate.now(), LocalDate.parse("9999-12-12"));
-        taskService.createTaskContainer(0, LocalDate.parse("9999-12-12"), TaskStatus.PROGRESS,
-                task.getTaskId());
+        taskService.createTaskContainer(1, LocalDate.parse("9999-12-12"), TaskStatus.PROGRESS, task.getTaskId());
     }
 
     @Given("a task {string} is marked as {string} by {string}, with email {string}")
     public void a_task_is_marked_as_by_with_email(String taskName, String status, String studentName,
             String studentEmail) {
         try {
-            final TaskContainer taskContainer = taskService.getTaskContainer(0);
-            taskContainer.setStatus(TaskStatus.valueOf(status.toUpperCase()));
-            taskContainerRepository.save(taskContainer);
+            final TaskContainer taskContainer = taskService.getTaskContainer(1);
 
             final Student student = studentService.createStudent(studentName, studentEmail, "password");
             Set<TaskContainer> schoolTasks = student.getSchoolTask();
             schoolTasks.add(taskContainer);
             studentRepository.save(student);
+
+            taskContainer.setStatus(TaskStatus.valueOf(status.toUpperCase()));
+            taskContainerRepository.save(taskContainer);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     @Given("a task {string} is not marked as done by {string}, with email {string}")
-    public void a_task_is_not_marked_as_done_by_with_email(String string, String string2, String string3) {
-        // it's already at in done status
+    public void a_task_is_not_marked_as_done_by_with_email(String taskName, String studentName, String studentEmail) {
+        try {
+            TaskContainer taskContainer = taskService.getTaskContainer(1);
+            final Student student = studentService.createStudent(studentName, studentEmail, "password");
+        
+            Set<TaskContainer> schoolTasks = student.getSchoolTask();
+            schoolTasks.add(taskContainer);
+            studentRepository.save(student);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     @When("{string} marks {string} as officially complete")
     public void marks_as_officially_complete(String teacherName, String taskName) throws Exception {
-        todopalRestController.approveTask("email@email.email", 0);
+        todopalRestController.approveTask("taha@pattomail.com", 1);
     }
 
     @Then("{string} will be marked as {string}")
     public void will_be_marked_as(String taskName, String taskStatus) {
         try {
-            TaskContainer taskContainer = taskService.getTaskContainer(0);
-            Assertions.assertEquals(TaskStatus.CLOSED, taskContainer.getStatus());
+            TaskContainer taskContainer = taskService.getTaskContainer(1);
+            Assertions.assertEquals(TaskStatus.valueOf(taskStatus.toUpperCase()), taskContainer.getStatus());
         } catch (Exception e) {
             e.printStackTrace();
         }
