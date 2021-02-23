29e659d8f67faffd1d14a021fb7a86bc277a9e66
nafiz1001 <nafiz.islam1001@gmail.com>
2021-02-23 17:34:44 -0500

    implement TestConfirmStudentTaskCompletion.java

--- todopal-backend/src/test/java/todopal/features/TestConfirmStudentTaskCompletion.java
+++ todopal-backend/src/test/java/todopal/features/TestConfirmStudentTaskCompletion.java
@@ -7,12 +7,15 @@ import java.util.Map;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
 import todopal.controller.TodopalRestController;
+import todopal.dao.StudentRepository;
+import todopal.dao.TaskContainerRepository;
 import todopal.dao.TaskRepository;
 import todopal.model.Student;
 import todopal.model.Task;
@@ -36,35 +39,65 @@ public class TestConfirmStudentTaskCompletion {
     @Autowired
     private TaskService taskService;
 
-    Exception errorThrown = new IllegalArgumentException("");
+    @Autowired
+    private StudentRepository studentRepository;
+    @Autowired
+    private TaskContainerRepository taskContainerRepository;
 
-    @Given("{string} created a classroom")
-    public void created_a_classroom(String string) {
-    }
     @Given("there is a class task called {string} that is worth {string} points")
     public void there_is_a_class_task_called_that_is_worth_points(String taskName, String points) {
+        final Task task = taskService.createTask(0, taskName, "", "chem", "mandatory", true, Integer.parseInt(points),
+                LocalDate.now(), LocalDate.parse("9999-12-12"));
+        taskService.createTaskContainer(0, LocalDate.parse("9999-12-12"), TaskStatus.PROGRESS,
+                task.getTaskId());
     }
+
     @Given("a task {string} is marked as {string} by {string}, with email {string}")
     public void a_task_is_marked_as_by_with_email(String taskName, String status, String studentName,
             String studentEmail) {
+        try {
+            final TaskContainer taskContainer = taskService.getTaskContainer(0);
+            taskContainer.setStatus(TaskStatus.valueOf(status.toUpperCase()));
+            taskContainerRepository.save(taskContainer);
+
+            final Student student = studentService.createStudent(studentName, studentEmail, "password");
+            Set<TaskContainer> schoolTasks = student.getSchoolTask();
+            schoolTasks.add(taskContainer);
+            studentRepository.save(student);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    @Given("task {string} is worth {string} points")
-    public void task_is_worth_points(String string, String string2) {
-    }
+
     @Given("a task {string} is not marked as done by {string}, with email {string}")
     public void a_task_is_not_marked_as_done_by_with_email(String string, String string2, String string3) {
+        // it's already at in done status
     }
 
     @When("{string} marks {string} as officially complete")
-    public void marks_as_officially_complete(String string, String string2) {
+    public void marks_as_officially_complete(String teacherName, String taskName) throws Exception {
+        todopalRestController.approveTask("email@email.email", 0);
     }
 
     @Then("{string} will be marked as {string}")
-    public void will_be_marked_as(String string, String string2) {
-        throw new io.cucumber.java.PendingException();
+    public void will_be_marked_as(String taskName, String taskStatus) {
+        try {
+            TaskContainer taskContainer = taskService.getTaskContainer(0);
+            Assertions.assertEquals(TaskStatus.CLOSED, taskContainer.getStatus());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
     @Then("{string} points will be added to the tally of student {string}")
-    public void points_will_be_added_to_the_tally_of_student(String string, String string2) {
-        throw new io.cucumber.java.PendingException();
+    public void points_will_be_added_to_the_tally_of_student(String point, String studentName) {
+        try {
+            Student student = studentService.getStudent("taha@pattomail.com");
+            Assertions.assertEquals(Integer.parseInt(point), student.getTotalPoints());
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
     }
 }
