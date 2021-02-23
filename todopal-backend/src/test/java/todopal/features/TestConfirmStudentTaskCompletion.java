57747a534c10178e874961484a5de152b089ccea
nafiz1001 <nafiz.islam1001@gmail.com>
2021-02-23 16:38:17 -0500

    update TestConfirmStudentTaskCompletion.java

--- todopal-backend/src/test/java/todopal/features/TestConfirmStudentTaskCompletion.java
+++ todopal-backend/src/test/java/todopal/features/TestConfirmStudentTaskCompletion.java
@@ -1,8 +1,10 @@
 package todopal.features;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -12,8 +14,13 @@ import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
 import todopal.controller.TodopalRestController;
 import todopal.dao.TaskRepository;
+import todopal.model.Student;
 import todopal.model.Task;
+import todopal.model.TaskContainer;
+import todopal.model.TaskStatus;
+import todopal.model.Teacher;
 import todopal.service.ClassroomService;
+import todopal.service.StudentService;
 import todopal.service.TaskService;
 import todopal.service.TeacherService;
 
@@ -23,14 +30,23 @@ public class TestConfirmStudentTaskCompletion {
     @Autowired
     private TeacherService teacherService;
     @Autowired
+    private StudentService studentService;
+    @Autowired
     private ClassroomService classroomService;
     @Autowired
     private TaskService taskService;
 
     Exception errorThrown = new IllegalArgumentException("");
 
+    @Given("{string} created a classroom")
+    public void created_a_classroom(String string) {
+    }
+    @Given("there is a class task called {string} that is worth {string} points")
+    public void there_is_a_class_task_called_that_is_worth_points(String taskName, String points) {
+    }
     @Given("a task {string} is marked as {string} by {string}, with email {string}")
-    public void a_task_is_marked_as_by_with_email(String string, String string2, String string3, String string4) {
+    public void a_task_is_marked_as_by_with_email(String taskName, String status, String studentName,
+            String studentEmail) {
     }
     @Given("task {string} is worth {string} points")
     public void task_is_worth_points(String string, String string2) {
