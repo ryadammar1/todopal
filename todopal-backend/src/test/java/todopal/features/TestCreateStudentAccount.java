368beacc5a973828951d105cd3e0bc33d920b011
BarrothTheDragon <delia_bretan@yahoo.com>
2021-02-23 19:37:41 -0500

    cleanup tests

--- todopal-backend/src/test/java/todopal/features/TestCreateStudentAccount.java
+++ todopal-backend/src/test/java/todopal/features/TestCreateStudentAccount.java
@@ -1,10 +1,10 @@
 package todopal.features;
 
-import io.cucumber.java.en.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 import todopal.controller.TodopalRestController;
-import todopal.model.Student;
-import todopal.service.ClassroomService;
 import todopal.service.StudentService;
 
 public class TestCreateStudentAccount {
@@ -12,30 +12,27 @@ public class TestCreateStudentAccount {
     TodopalRestController todopalRestController;
     @Autowired
     private StudentService studentService;
-    @Autowired
-    private ClassroomService classroomService;
 
     @Given("an unregistered student wants to register for a new account")
     public void an_unregistered_student_wants_to_register_for_a_new_account() {
 
     }
 
-
     @When("user {string} with email {string} registers for a student account with name {string} and password {string}")
-    public void user_with_email_registers_for_a_student_account_with_name_and_password(String name, String email, String accountName, String password) {
-        try{
-            Student student = studentService.createStudent(name, email, password);
-            System.out.println(student.getEmail());
-        }catch (Exception e){
+    public void user_with_email_registers_for_a_student_account_with_name_and_password(String name, String email,
+            String accountName, String password) {
+        try {
+            studentService.createStudent(name, email, password);
+        } catch (Exception e) {
             Ressources.message = e.getMessage();
         }
     }
 
     @Given("a registered student {string} with email {string} wants to register for a new account")
     public void a_registered_student_with_email_wants_to_register_for_a_new_account(String name, String email) {
-        try{
+        try {
             studentService.createStudent(name, email, "1234");
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new AssertionError();
         }
     }
