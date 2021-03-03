850e381dc3f4a1a098e5c2c5edb5eb2737a6aa70
Ryad Ammar <mohamed.ammar@mail.mcgill.ca>
2021-03-03 16:13:45 -0500

    T2

--- todopal-backend/src/test/java/todopal/service/TestStudentService.java
+++ todopal-backend/src/test/java/todopal/service/TestStudentService.java
@@ -2,7 +2,7 @@ package todopal.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.lenient;
 
