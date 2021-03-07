c4f791b4e6edea204ecf3552bd5e562f3bfcaed7
BarrothTheDragon <delia_bretan@yahoo.com>
2021-03-07 16:29:04 -0500

    dao refac

--- todopal-backend/src/main/java/todopal/dao/TaskContainerRepository.java
+++ todopal-backend/src/main/java/todopal/dao/TaskContainerRepository.java
@@ -1,6 +1,7 @@
 package todopal.dao;
 
 import org.springframework.data.repository.CrudRepository;
+
 import todopal.model.TaskContainer;
 
 public interface TaskContainerRepository extends CrudRepository<TaskContainer, Long> {
