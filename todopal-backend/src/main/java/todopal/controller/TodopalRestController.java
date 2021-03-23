8221ad08153c67665467b06a3dd2620b9454cc13
Zimu Su <s529444757@gmail.com>
2021-03-23 17:56:36 -0400

    fixed typo.

--- todopal-backend/src/main/java/todopal/controller/TodopalRestController.java
+++ todopal-backend/src/main/java/todopal/controller/TodopalRestController.java
@@ -184,7 +184,7 @@ public class TodopalRestController {
 		return teachers;
 	}
 
-	@PostMapping(value = { "/students/{name}", "/studentss/{name}/" })
+	@PostMapping(value = { "/students/{name}", "/students/{name}/" })
 	public StudentDto createStudent(@PathVariable("name") String name, @RequestParam("email") String email,
 			@RequestParam("password") String password) throws IllegalArgumentException {
 		Student student = studentService.createStudent(name, email, password);
@@ -196,18 +196,21 @@ public class TodopalRestController {
 		Student student = studentService.getStudent(email);
 		return Converter.convertToDto(student);
 	}
-	
+
 	@GetMapping(value = { "/student-school-task/{email}", "/student-school-task/{email}/" })
 	public List<TaskContainerDto> getStudentSchoolTaskContainers(@PathVariable("email") String email) throws Exception {
 		List<TaskContainerDto> taskContainers = new ArrayList<TaskContainerDto>();
-		studentService.getStudentSchoolTasks(email).forEach(container -> taskContainers.add(Converter.convertToDto(container)));
+		studentService.getStudentSchoolTasks(email)
+				.forEach(container -> taskContainers.add(Converter.convertToDto(container)));
 		return taskContainers;
 	}
-	
+
 	@GetMapping(value = { "/student-personal-task/{email}", "/student-personal-task/{email}/" })
-	public List<TaskContainerDto> getStudentPersonalTaskContainers(@PathVariable("email") String email) throws Exception {
+	public List<TaskContainerDto> getStudentPersonalTaskContainers(@PathVariable("email") String email)
+			throws Exception {
 		List<TaskContainerDto> taskContainers = new ArrayList<TaskContainerDto>();
-		studentService.getStudentPersonalTasks(email).forEach(container -> taskContainers.add(Converter.convertToDto(container)));
+		studentService.getStudentPersonalTasks(email)
+				.forEach(container -> taskContainers.add(Converter.convertToDto(container)));
 		return taskContainers;
 	}
 
