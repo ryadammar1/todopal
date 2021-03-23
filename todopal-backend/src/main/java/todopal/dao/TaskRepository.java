5f9c6db1a3a0c06be893e2f6c48e89206eabf30f
TahaLazreg <tlazreg1@gmail.com>
2021-03-23 12:21:02 -0400

    Added business methods

--- todopal-backend/src/main/java/todopal/dao/TaskRepository.java
+++ todopal-backend/src/main/java/todopal/dao/TaskRepository.java
@@ -2,8 +2,11 @@ package todopal.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
+import todopal.model.Classroom;
 import todopal.model.Task;
 
 public interface TaskRepository extends CrudRepository<Task, Long> {
 	Task findBytaskId(long taskId);
+	Task deleteTaskByTaskId(long taskId);
+	Task findTaskByNameAndAndClassroom(String name, Classroom classroom);
 }
