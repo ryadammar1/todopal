ca7f25d152d67f72dcbce2c18b533ecff7d41c82
Ricky Liu <li.qi.liu@mail.mcgill.ca>
2021-03-24 17:01:05 -0400

    db clearing

---
+++ todopal-backend/src/test/java/todopal/controller/DBCleaner.js
@@ -0,0 +1,17 @@
+import pg from 'pg';
+
+
+export const clean = async () => {
+    const Pool = pg.Pool;
+    const connectionString = process.env.CONNECTION_URI
+    const pool = new Pool({connectionString, ssl: true});
+    await pool.query('DELETE FROM student_personal_task');
+    await pool.query('DELETE FROM student_school_task');
+    await pool.query('DELETE FROM student');
+    await pool.query('DELETE FROM task_container');
+    await pool.query('DELETE FROM task');
+    await pool.query('DELETE FROM teacher_classroom');
+    await pool.query('DELETE FROM classroom');
+    await pool.query('DELETE FROM teacher');
+    pool.end();
+}
--- todopal-backend/src/test/java/todopal/controller/IntegrationTests.js
+++ todopal-backend/src/test/java/todopal/controller/IntegrationTests.js
@@ -1,8 +1,12 @@
 /* Here is the integration testing main script. Run this and the files with the axios calls will run. */
 
+//dotenv config
+import dotenv from 'dotenv';
+dotenv.config();
+
 //axios config, dw about this
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = process.env.BASE_URL;
 
 //import assertions
 import assert from 'assert';
@@ -10,7 +14,11 @@ import assert from 'assert';
 //import your test files here
 import { ID012 } from './ID012.js';
 
+//import db cleaner
+import { clean } from './DBCleaner.js';
+
 //call your test files here
 const id012 = await ID012();
+clean();
 
 assert.strictEqual("Kevin", id012[0]);
